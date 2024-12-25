9481c61d610ac5f44dee04ae5bb2f6bb303e3453
Victor Surikov <virasio@gmail.com>
2024-12-25 08:26:36 +0300

    Xcode project initial commit

---
+++ NavigationPlayground/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  NavigationPlayground
+//
+//  Created by Victor Surikov on 24.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .font(.title)
+    }
+
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NavigationPlayground/NavigationPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationPlaygroundApp.swift
+//  NavigationPlayground
+//
+//  Created by Victor Surikov on 24.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
